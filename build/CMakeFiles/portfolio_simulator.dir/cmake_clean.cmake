f6fff8d2e48991a7ffb541c01d31517aad9495ac
Renal Sabirov <roman.sabirov12345@gmail.com>
2025-04-04 16:52:18 +0300

    --Cmake things

---
+++ build/CMakeFiles/portfolio_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "C:/Users/azathd/Documents/Projects/portfoliooptimiser/portfolio_simulator.exe"
+  "C:/Users/azathd/Documents/Projects/portfoliooptimiser/portfolio_simulator.exe.manifest"
+  "C:/Users/azathd/Documents/Projects/portfoliooptimiser/portfolio_simulator.pdb"
+  "CMakeFiles/portfolio_simulator.dir/src/investinstrument/stocks.cpp.obj"
+  "CMakeFiles/portfolio_simulator.dir/src/investinstrument/stocks.cpp.obj.d"
+  "CMakeFiles/portfolio_simulator.dir/src/main.cpp.obj"
+  "CMakeFiles/portfolio_simulator.dir/src/main.cpp.obj.d"
+  "CMakeFiles/portfolio_simulator.dir/src/pricing/price_simulator.cpp.obj"
+  "CMakeFiles/portfolio_simulator.dir/src/pricing/price_simulator.cpp.obj.d"
+  "libportfolio_simulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/portfolio_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
